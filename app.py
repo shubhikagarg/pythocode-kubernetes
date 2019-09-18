f26a33d8fe5cd5be95db4822ece6c5f63f5ec7c1
Shubhika <shubhikag@cybage.com>
2019-09-18 14:11:17 +0530

    inital commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template,request,redirect,url_for
+import socket
+import platform
+import os
+
+app = Flask(__name__)
+
+
+#getting the details
+@app.route('/machineinfo')
+
+def machineinfo():
+    #get machine details
+    host_name = socket.gethostname()
+    host_ip = socket.gethostbyname(host_name)
+    os_name = platform.system()
+    mycolor = (os.environ['color'])
+
+
+
+    return render_template("machineinfo.html", host_name=host_name, host_ip=host_ip, os_name=os_name,mycolor=mycolor)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9000)
+
